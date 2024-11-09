8757c9f0a5d9dcb067add0290d06f39c911c08c6
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-11-09 22:47:00 +0100

    .cmd files now improved

--- apps/app01/launch--no-console.cmd
+++ apps/app01/launch--no-console.cmd
@@ -1,6 +1,13 @@
-REM cd sdl-lua/apps/app01/ && sdl-lua/sdl-lua.exe
-REM gcc src/*.c -o sdl-lua-windows $(pkg-config --cflags --libs sdl2) -mwindows
-set ROOT_PATH=%CD%\..\..
-set PATH=%ROOT_PATH%;%PATH%
-start %ROOT_PATH%\sdl-lua-windows
-REM pause
+@REM cd sdl-lua/apps/app01/ && sdl-lua/sdl-lua.exe
+@REM gcc src/*.c -o sdl-lua-windows $(pkg-config --cflags --libs sdl2) -mwindows
+
+@echo off
+
+@SET my_path=%~dp0
+@SET this_dir=%my_path:~0,-1%
+CD "%this_dir%"
+
+SET ROOT_PATH=%this_dir%\..\..
+@SET PATH=%ROOT_PATH%;%PATH%
+START "windows" "%ROOT_PATH%\sdl-lua-windows"
+REM PAUSE
