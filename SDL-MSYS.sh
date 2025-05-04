6a061ebbb46f2da4e656f444adde1878a81b571f
Dario Cangialosi <dario.cangialosi@gmail.com>
2025-05-04 17:12:11 +0200

    [build] switch from gcc to clang for compiling sdl-lua binaries

--- SDL-MSYS.sh
+++ SDL-MSYS.sh
@@ -2,8 +2,8 @@
 
 LJDIR=c:/MSYS64/home/dario/luajit/src
 
-gcc src/*.c -o sdl-lua-console $(pkg-config --cflags --libs sdl3) -I$LJDIR -L$LJDIR -lluajit-5.1.dll -mconsole
-gcc src/*.c -o sdl-lua-windows $(pkg-config --cflags --libs sdl3) -I$LJDIR -L$LJDIR -lluajit-5.1.dll -mwindows
+clang src/*.c -o sdl-lua-console $(pkg-config --cflags --libs sdl3) -I$LJDIR -L$LJDIR -lluajit-5.1.dll -mconsole
+clang src/*.c -o sdl-lua-windows $(pkg-config --cflags --libs sdl3) -I$LJDIR -L$LJDIR -lluajit-5.1.dll -mwindows
 
 cp /c/msys64/mingw64/bin/SDL3.dll .
 cp $LJDIR/lua51.dll .
