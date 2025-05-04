5c17abe179c7e5e75b3e22c42e63f6ea0e9d8266
Dario Cangialosi <dario.cangialosi@gmail.com>
2025-05-04 16:59:36 +0200

    [build] update SDL references from SDL2 to SDL3 in SDL-MSYS.sh

--- SDL-MSYS.sh
+++ SDL-MSYS.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-LUAJ=c:/MSYS64/home/dario/luajit/src
+LJDIR=c:/MSYS64/home/dario/luajit/src
 
-gcc src/*.c -o sdl-lua-console $(pkg-config --cflags --libs sdl2) -I$LUAJ -L$LUAJ -lluajit-5.1.dll -mconsole
-gcc src/*.c -o sdl-lua-windows $(pkg-config --cflags --libs sdl2) -I$LUAJ -L$LUAJ -lluajit-5.1.dll -mwindows
+gcc src/*.c -o sdl-lua-console $(pkg-config --cflags --libs sdl3) -I$LJDIR -L$LJDIR -lluajit-5.1.dll -mconsole
+gcc src/*.c -o sdl-lua-windows $(pkg-config --cflags --libs sdl3) -I$LJDIR -L$LJDIR -lluajit-5.1.dll -mwindows
 
-cp /c/msys64/mingw64/bin/SDL2.dll .
-cp /c/msys64/mingw64/bin/lua51.dll .
+cp /c/msys64/mingw64/bin/SDL3.dll .
+cp $LJDIR/lua51.dll .
